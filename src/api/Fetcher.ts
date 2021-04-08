0f30867230ba3ba38131dd1b614e696a174884fc
Anton Aksionau <ikari808@gmail.com>
2021-04-08 13:06:43 +0200

    add api url to fetcher

--- src/api/Fetcher.ts
+++ src/api/Fetcher.ts
@@ -1,13 +1,16 @@
 export class Fetcher {
     token: string;
+    apiUrl: string;
     constructor() {
         console.log("fetcher", this.getToken())
         this.token = this.getToken();
+        this.apiUrl = (process as any).env.API_URL;
     }
 
     async fetch<I>(url: string, options: RequestInit = {}): Promise<{ data: I, res: Response }> {
         console.log("fetcher token", this.token)
         console.log(this)
+
         if (this.token) {
             options.headers = {
                 ...options.headers,
@@ -16,7 +19,7 @@ export class Fetcher {
         }
 
         return new Promise(async (done, rej) => {
-            const res = await fetch(url, options);
+            const res = await fetch(url.replace("/api/", `${this.apiUrl}/`), options);
 
             if (res.ok) {
                 const data: I = await res.json();
